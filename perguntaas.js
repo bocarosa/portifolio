2be43ba2454dd6faeea5c93f4164493abe77a273
Bianca <142339320+bocarosa@users.noreply.github.com>
2025-06-17 11:59:09 -0300

    fg

---
+++ perguntaas.js
@@ -0,0 +1,14 @@
+prova(
+    'Russo',
+    'Como se fala poderia em russo?',
+    'это могло бы')
+
+recado(
+    'Russo',
+    'Como pedir para um garçom te trazer água?',
+    'Можно мне стакан воды?')
+
+criacartao(
+    'Russo',
+    'Como fala "quem" em russo',
+    'ВОЗ')
